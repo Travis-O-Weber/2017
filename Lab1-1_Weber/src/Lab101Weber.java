4faff1c4755201f701803dd3e250a1ab1bc34e5f
Travis-O-Weber <33355911+Travis-O-Weber@users.noreply.github.com>
2025-02-01 21:34:44 -0600

    Add files via upload

---
+++ Lab1-1_Weber/src/Lab101Weber.java
@@ -0,0 +1,12 @@
+
+public class Lab101Weber 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Wecome To Java!");
+		System.out.println("Wecome To Computer Science!");
+		System.out.println("Programming is Fun!");
+	}
+
+}
---
+++ lab1-2_Weber/src/lab102Weber.java
@@ -0,0 +1,14 @@
+
+public class lab102Weber 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Wecome To Java!");
+		System.out.println("Wecome To Java!");
+		System.out.println("Wecome To Java!");
+		System.out.println("Wecome To Java!");
+		System.out.println("Wecome To Java!");
+	}
+
+}
